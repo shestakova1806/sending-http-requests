837b92afc179b14c29ae7d0a4b417d8044410aa4
Alexandra Shestakova <shestakova18@bk.ru>
2022-03-20 20:37:50 +0300

    added sending POST request

--- src/App.js
+++ src/App.js
@@ -14,23 +14,28 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch("https://react-http-9c72e-default-rtdb.europe-west1.firebasedatabase.app/movies.json");
+      const response = await fetch(
+        "https://react-http-9c72e-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
 
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -41,8 +46,19 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const responce = await fetch(
+      "https://react-http-9c72e-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+    const data = await responce.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
